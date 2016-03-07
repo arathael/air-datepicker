a314b01ef61c59c71a57283119f5de2dc72351d2
Ram Ram <ramon@www.com.mx>
2016-03-07 16:46:24 -0600

    add: es lang

---
+++ src/js/i18n/datepicker.es.js
@@ -0,0 +1,11 @@
+Datepicker.language['es'] = {
+    days: ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
+    daysShort: ['Dom', 'Lun', 'Mar', 'Mier', 'Jue', 'Vie', 'Sab'],
+    daysMin: ['Do', 'Lu', 'Ma', 'Mi', 'Ju', 'Vi', 'Sa'],
+    months: ['Enero','Febrero','Marzo','Abril','Mayo','Junio', 'Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'],
+    monthsShort: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'],
+    today: 'Hoy',
+    clear: 'Borrar',
+    dateFormat: 'dd/mm/yy',
+    firstDay: 0
+};
